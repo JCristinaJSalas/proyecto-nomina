9248b2a276d392d41757ba9081a087e9fb129906
JCristinaJSalas <criistiinajerez@gmail.com>
2023-09-20 13:20:10 -0500

    feat: :sparkles: Add Calcular function
    Calcula la diferencia de los egresos con los ingresos

--- modulos/api.js
+++ modulos/api.js
@@ -1,13 +1,9 @@
-let formNomina = document.querySelector("form");
-
 //Save information
-
 export const saveData = (urlNomina) => {
+  let formNomina = document.querySelector("form");
   formNomina.addEventListener("submit", async (e) => {
+    e.preventDefault();
     const data = Object.fromEntries(new FormData(e.target));
-    const { monto } = data;
-    Number(monto);
-
     let config = {
       method: "POST",
       headers: { "content-type": "application/json" },
@@ -15,28 +11,39 @@ export const saveData = (urlNomina) => {
     };
     let res = await (await fetch(urlNomina, config)).json();
     console.log(res);
+    location.reload();
   });
 };
-
 // Eliminar
-
-const deleteData = async(urlNomina, id) => {
-  console.log(id)
+const deleteData = async (urlNomina, id) => {
+  console.log(id);
   let config = {
     method: "DELETE",
-    headers: { "content-type": "application/json" }
+    headers: { "content-type": "application/json" },
   };
-  let res = await (await fetch(urlNomina+`/${id}`, config)).json();
-  location.reload()
+  let res = await (await fetch(urlNomina + `/${id}`, config)).json();
+  location.reload();
+};
+// Calcular
+const calcular = (total) => {
+  console.log(total);
+  document.querySelector('#trFooter').insertAdjacentHTML(
+    'beforeend', /*html*/ `
+    <td>${total}</td>
+    `
+  )
 
-}
 
-
+};
 // Read information
 export const writeData = (urlNomina) => {
   addEventListener("DOMContentLoaded", async () => {
     let res = await (await fetch(urlNomina)).json();
+    let total = 0;
     res.map((element) => {
+      element.eleccion === "ingreso"
+        ? (total += parseInt(element.monto))
+        : (total -= parseInt(element.monto));
       document.querySelector("#tabla").insertAdjacentHTML(
         "beforeend",
         /*html*/ `
@@ -54,7 +61,7 @@ export const writeData = (urlNomina) => {
     // Agregar event listeners para botones de edición y eliminación
     const botonesEditar = document.querySelectorAll(".edit");
     const botonesEliminar = document.querySelectorAll(".delete");
-
+    calcular(total);
     botonesEditar.forEach((button) => {
       button.addEventListener("click", (e) => {
         e.preventDefault();
@@ -70,12 +77,7 @@ export const writeData = (urlNomina) => {
         // Aquí puedes llamar a la función deleteData para eliminar la información con el ID proporcionado
         console.log(`Delete elemento con ID ${id}`);
         deleteData(urlNomina, id);
-        
       });
     });
-
-
   });
 };
-
-
