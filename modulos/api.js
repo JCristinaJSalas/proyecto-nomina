5d8d44e0b98310af88ef6173ab020cb47361c8cd
JCristinaJSalas <89360648+JCristinaJSalas@users.noreply.github.com>
2023-09-21 00:33:08 -0500

    feat: :sparkles: Working on Editar function

--- modulos/api.js
+++ modulos/api.js
@@ -1,6 +1,6 @@
 //Save information
 export const saveData = (urlNomina) => {
-  let formNomina = document.querySelector("form");
+  let formNomina = document.querySelector("#form-fill");
   formNomina.addEventListener("submit", async (e) => {
     e.preventDefault();
     const data = Object.fromEntries(new FormData(e.target));
@@ -26,23 +26,93 @@ const deleteData = async (urlNomina, id) => {
 };
 // Calcular
 const calcular = (total) => {
-  document.querySelector('#trFooter').insertAdjacentHTML(
-    'beforeend', /*html*/ `
+  document.querySelector("#trFooter").insertAdjacentHTML(
+    "beforeend",
+    /*html*/ `
     <td>${total}</td>
     `
-  )
-
-
+  );
 };
-//Editar
-const editar = async(id, urlNomina) => {
-  let data = await (await fetch(urlNomina)).json();
-  const newData = data.find((e) => {
-    console.log(e)
-  })
-  console.log(`Editar elemento con ID ${id}`, newData);
+//Editar funcion
 
-}
+const editar = async (id, urlNomina) => {
+  try {
+    // Obtener los datos de la transacción con el ID especificado
+    const response = await fetch(`${urlNomina}/${id}`);
+    if (!response.ok) {
+      console.error("Error al obtener la transacción para editar");
+      return;
+    }
+    const transaction = await response.json();
+
+    // Mostrar el formulario de edición con los datos de la transacción
+    const formEdit = document.querySelector("#formEdit");
+    formEdit.innerHTML = `
+      <div><span class="material-symbols-outlined close">close</span></div>
+      <input placeholder="${transaction.monto}" id="monto">
+      <div class="container-radio">
+        <label for="radio-egreso">Egreso</label>
+        <input
+          type="radio"
+          name="eleccion"
+          id="radio-egreso"
+          required
+          ${transaction.tipo === "egreso" ? "checked" : ""}
+          value="egreso"
+        />
+      </div>
+      <div class="container-radio">
+        <label for="radio-ingreso">Ingreso</label>
+        <input
+          type="radio"
+          name="eleccion"
+          id="radio-ingreso"
+          required
+          ${transaction.tipo === "ingreso" ? "checked" : ""}
+          value="ingreso"
+        />
+      </div>
+      <input placeholder="${transaction.descripcion}">
+      <button type="submit">Guardar</button>
+    `;
+
+    // Escuchar el evento submit del formulario de edición
+    formEdit.addEventListener("submit", async (e) => {
+      e.preventDefault();
+      // Obtener los nuevos valores del formulario
+      const nuevoMonto = document.getElementById("monto").value;
+      const nuevoTipo = document.querySelector(
+        'input[name="eleccion"]:checked'
+      ).value;
+      const nuevaDescripcion = document.getElementById("descripcion").value;
+
+      // Crear un objeto con los datos actualizados
+      const newData = {
+        monto: nuevoMonto,
+        tipo: nuevoTipo,
+        descripcion: nuevaDescripcion,
+      };
+
+      // Enviar una solicitud PUT para actualizar la transacción
+      const response = await fetch(`${urlNomina}/${id}`, {
+        method: "PUT",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(newData),
+      });
+
+      if (response.ok) {
+        console.log("Transacción actualizada con éxito");
+        // Volver a cargar los datos y recalcular el total
+        loadData();
+        calculateTotal();
+      } else {
+        console.error("Error al actualizar la transacción");
+      }
+    });
+  } catch (error) {
+    console.error("Error en la función editar:", error);
+  }
+};
 
 // Read information
 export const writeData = (urlNomina) => {
@@ -53,6 +123,7 @@ export const writeData = (urlNomina) => {
       element.eleccion === "ingreso"
         ? (total += parseInt(element.monto))
         : (total -= parseInt(element.monto));
+
       document.querySelector("#tabla").insertAdjacentHTML(
         "beforeend",
         /*html*/ `
@@ -77,8 +148,8 @@ export const writeData = (urlNomina) => {
         e.preventDefault();
         const id = button.getAttribute("id");
         // Aquí puedes implementar la lógica para editar la información con el ID proporcionado
-        
-        editar(id, urlNomina)
+
+        editar(id, urlNomina);
       });
     });
     botonesEliminar.forEach((button) => {
@@ -92,4 +163,3 @@ export const writeData = (urlNomina) => {
     });
   });
 };
-
