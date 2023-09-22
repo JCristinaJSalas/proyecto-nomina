5b6b6d291cb7378ff4f918b4f2f36e68b713bb83
JCristinaJSalas <89360648+JCristinaJSalas@users.noreply.github.com>
2023-09-22 03:25:31 -0500

    feat: :sparkles: Function Edit working

--- modulos/editar.js
+++ modulos/editar.js
@@ -1,84 +1,102 @@
-
 const modal = document.querySelector(".modal");
-
+const formEditar = document.querySelector("#formEdit");
 //Editar funcion
 
 export const editar = async (id, urlNomina) => {
   const response = await (await fetch(`${urlNomina}/${id}`)).json();
-  document.querySelector("#formEdit").insertAdjacentHTML(
-    "beforeBegin",
+  formEditar.insertAdjacentHTML(
+    "beforeend",
     /*html*/ `
+    <div class="botonCerrar">
+      <box-icon name="x" class="cerrar" ></box-icon>
+    </div>
     <div class="input-container">
-        <label for="montoNuevo" class="label">Cantidad</label>
-        <input type="number" id="montoNuevo" name="monto" class="text_input" autocomplete="off"
-        value="${response.monto}" required />
+      <label for="monto" class="label">Cantidad</label>
+      <input
+        type="number"
+        id="montoNuevo"
+        name="monto"
+        class="text_input"
+        autocomplete="off"
+        value="${response.monto}"
+        required
+      />
     </div>
     <div class="contenedor-radios">
     <div class="container-radio">
-      <label for="radio-egreso">Egreso</label>
-
-      <input type="radio" name="eleccion" id="radio-egreso" required ${
-        response.eleccion === "egreso" ? "checked" : ""
-      }
-      value="egreso" />
-
-    </div>
-
-    <div class="container-radio">
-      <label for="radio/ingreso">Ingreso</label>
-      <input type="radio" name="eleccion" id="radio/ingreso" required ${
-        response.eleccion === "ingreso" ? "checked" : ""
-      }
-      value="ingreso"/>
-    </div>
+    <label for="radio-egreso">Egreso</label>
+    <input
+      type="radio"
+      name="eleccion-guardar"
+      id="radio-egreso"
+      required
+      value="egreso"
+      ${response.eleccion === "egreso" ? "checked" : ""}
+    />
   </div>
+
+  <div class="container-radio">
+    <label for="radio-ingreso">Ingreso</label>
+    <input
+      type="radio"
+      name="eleccion-guardar"
+      id="radio-ingreso"
+      required
+      value="ingreso"
+      ${response.eleccion === "ingreso" ? "checked" : ""}
+    />
+  </div>
+    </div>
+    <div class="container-descripcion">
+      <label for="descripcion">Descripcion</label>
+      <textarea
+        name="descripcion"
+        id="descripcionNueva"
+        cols="30"
+        rows="10"
+      >${response.descripcion}</textarea>
+    </div>
+    <div class="container-boton">
+    <input type="submit" value="EDITAR" />
+    </div>
+
     
-<br/>
-<div class="container-descripcion">
-        <label for="descripcion">Descripcion</label>
-        <textarea name="descripcion" id="descripcion" cols="30" rows="10" >${
-          response.descripcion
-        }</textarea>
-      </div>
-
-
-  </div>
-
   `
   );
+  const cerrar = document.querySelector(".botonCerrar");
 
-  document.querySelector("#formEdit").addEventListener("submit", async (e) => {
-    console.log(Object.entries(new FormData(  document.querySelector("#formEdit"))));
-    e.preventDefault();
-
-    const montoNuevo = document.querySelector("#montoNuevo").value;
-    const nuevoTipo = document.querySelector(
-      'input[name="eleccion"]:checked'
-    ).value;
-    const nuevaDescripcion = document.querySelector("#descripcionNueva").value;
-    console.log(montoNuevo)
-
-    const nuevaData = {
-      monto: montoNuevo,
-      tipo: nuevoTipo,
-      descripcion: nuevaDescripcion,
-    };
-
-    console.log(nuevaData);
-    let config = {
-      method: "PUT",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify(nuevaData),
-    };
-    let res = await (await fetch(urlNomina + `/${id}`, config)).json();
-    console.log(res);
-    location.reload();
+  formEditar.addEventListener("submit", () => {
+    saveEdit(id, urlNomina);
   });
 
-  document.querySelector(".botonCerrar").addEventListener("click", (e) => {
-    e.preventDefault();
-    modal.classList.remove("modalShow");
-    location.reload();
+  cerrar.addEventListener("click", () => {
+    cerrarBoton();
   });
 };
 
+const saveEdit = async (id, urlNomina) => {
+  const montoNuevo = document.querySelector("#montoNuevo").value;
+  const nuevoTipo = document.querySelector(
+    'input[name="eleccion-guardar"]:checked'
+  ).value;
+  const nuevaDescripcion = document.querySelector("#descripcionNueva").value;
+
+  const nuevaData = {
+    monto: montoNuevo,
+    eleccion: nuevoTipo,
+    descripcion: nuevaDescripcion,
+  };
+
+  let config = {
+    method: "PUT",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(nuevaData),
+  };
+  let res = await (await fetch(urlNomina + `/${id}`, config)).json();
+  location.reload();
+};
+
+const cerrarBoton = () => {
+  modal.classList.remove("modalShow");
+  location.reload();
+};
