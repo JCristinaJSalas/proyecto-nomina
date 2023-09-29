fa31e44e7650e8052ec8ff9c47e1f61bce4d0074
JCristinaJSalas <89360648+JCristinaJSalas@users.noreply.github.com>
2023-09-28 23:02:07 -0500

    feat: :fire: Delete some files to get a better modulation

--- modulos/delete.js
+++
@@ -1,9 +0,0 @@
-// Eliminar
-export const deleteData = async (urlNomina, idDelete) => {
-  let config = {
-    method: "DELETE",
-    headers: { "content-type": "application/json" },
-  };
-  let res = await (await fetch(urlNomina + `/${idDelete}`, config)).json();
-  location.reload();
-};
--- modulos/diferencia.js
+++
@@ -1,9 +0,0 @@
-// Calcular
-export const calcular = (total) => {
-    document.querySelector("#trFooter").insertAdjacentHTML(
-      "beforeend",
-      /*html*/ `
-      <td>$ ${total.toLocaleString("es-ES")}</td>
-      `
-    );
-  };
